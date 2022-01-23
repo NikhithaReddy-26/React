0fa61de56c0e9ef827801ca6a797329201f86d79
NikhithaReddy-26 <63488411+NikhithaReddy-26@users.noreply.github.com>
2022-01-23 12:41:22 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import test from './test';
+import Apple from './Apple';
+class App extends React.Component{
+  state = {
+   name: "nikhitha",
+   age: 20, 
+   from: "mbnr"
+  }
+  render()
+  {
+    return(
+      <div>
+        <center>
+            <Apple name={this.state.name} age= {this.state.age} from={this.state.from}/>
+       </center>
+      </div>
+    
+    )}
 }
-
 export default App;
---
+++ src/Apple.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Apple extends React.Component{
+    render()
+    {
+        return(
+            <div><center><h2>Name={this.props.name} Age={this.props.age} From={this.props.} </h2></center></div>
+        )
+    }
+}
+export default Apple;
